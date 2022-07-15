6494accae140f1446a66bcec9701ca60a323a9a3
Denver <denvercowan@rocketmail.com>
2022-07-15 10:46:12 -0400

    completed 02 & 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) return "ERROR"; //more of an edge case situation here IMO
+  let words = ""; //I wasn't letting it = an empty string
+  for (let i = 0; i < num; i++) {
+    //I also wasn't using let i
+    words += string;
+  }
+  return words;
 };
 
 // Do not edit below this line
